6c2fbf2895d556738216e9578589542cd1ef501b
Derek O'Callaghan <derek.ocallaghan@parameterspace.ie>
2016-02-11 12:22:58 +0000

    948 - removed deprecated SR AVI

--- SR_avi/avi/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-#from avi.tests.test_index import *
--- SR_avi/avi/tests/test_models.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-from avi.models import DemoModel
-from django.core.urlresolvers import reverse
-from pipeline.models import AviJobRequest, PipeState
-
-# Create your tests here.
