3226233403523f1542e87686b141ad7301a0d09d
Derek O'Callaghan <derek.ocallaghan@parameterspace.ie>
2016-03-09 15:23:09 +0000

    Added working example of a Spark AVI pipeline, that relies on a mounted Spark installation. It currently runs one of the examples provided with the Spark bundle. It is completely REST-based, i.e. no views.py, and no static, templates etc. directories. @issue: 760 @time: 3.0 @progress: 90 @activity: Development Branch: develop
    modified:   spark_avi/avi/tasks.py
    modified:   spark_avi/avi/urls.py
    deleted:    spark_avi/avi/views.py
    modified:   spark_avi/avi/views_api.py

--- spark_avi/avi/tasks.py
+++ spark_avi/avi/tasks.py
@@ -3,29 +3,42 @@ GAVIP Example AVIS: Spark AVI
 
 This pipeline demonstrates how a job can be submitted to a Spark cluster
 '''
-
 import hashlib
 import json
 import logging
-import operator
 import os
-import subprocess
+from operator import add
+from random import random
 
-# Class used for creating pipeline tasks
+# Classes used for creating pipeline tasks
+from luigi.contrib.spark import PySparkTask
 from pipeline.classes import AviTask, AviParameter, AviLocalTarget
 
 from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
-class RunSpark(AviTask):
+class RunSpark(AviTask, PySparkTask):
     """
-    Run one of the example Spark jobs
+    Runs an example Spark job. Note that multiple inheritance of both 
+    AviTask and SparkSubmitTask/PySparkTask is required.
+
+    'spark-submit' location is currently configured in /opt/gavip_avi/luigi.cfg,
+    which should either:
+    
+    1) match a volume that is mounted to the Spark location when the AVI container is created
+
+    or
+
+    2) match a volume within the container (i.e. with a Spark template that builds on the Java template)
     """
 
     def __init__(self, *args, **kwargs):
         super(RunSpark, self).__init__(*args, **kwargs)
-        param_values = 'TODO'
+        # A typical Spark job will have multiple input parameters,
+        # which can be used to generate a unique hash for the task outputs.
+        # This simple task currently requires no parameters.
+        param_values = 'SparkJob'
         self.output_file_prefix = hashlib.md5(param_values).hexdigest()
         logger.info('output_file_prefix: %s' % (self.output_file_prefix))
 
@@ -34,20 +47,35 @@ class RunSpark(AviTask):
 
     def output(self):
         return AviLocalTarget(os.path.join(settings.OUTPUT_PATH, self.get_output_filename()))
+    
+    def main(self, sc, *args):
+        """
+        Spark tasks implement a main() function, instead of the standard run() function used with AviTask.
+        For now, print() calls should be made in this function, instead of using logger, as main() is 
+        executed within Spark, outside of Django.
 
-    def run(self):
+        Pi calculation, taken from examples/src/main/python/pi.py provided with Spark 1.6.0
         """
-        Simple wordcount - TODO: add URL
-        """
-        logger.info('Running Spark Job')
+        print('Running Spark Job - Pi Calculation')
         
-        analysis_context = {'result': True,
-                            }
+        partitions =  2
+        n = 100000 * partitions
 
-        logger.debug(analysis_context)
-        # JSON will be the context used for the template
+        def f(_):
+            x = random() * 2 - 1
+            y = random() * 2 - 1
+            return 1 if x ** 2 + y ** 2 < 1 else 0
+
+        count = sc.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+        calculated_pi = (4.0 * count / n)
+        print("Calculated Pi is roughly %f" % calculated_pi)
+
+        # JSON output for subsequent analysis
+        spark_result = {'pi': calculated_pi}
         with open(self.output().path, 'wb') as out:
-            json.dump(analysis_context, out)
+            json.dump(spark_result, out)
+
+        print('Completed Spark Job, result: %s' % spark_result)
 
 
 
--- spark_avi/avi/urls.py
+++ spark_avi/avi/urls.py
@@ -6,7 +6,7 @@ These URLs are used by the AVI web-interface.
 from django.conf.urls import include, patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from plugins.urls import plugins_urls
-from avi import views, views_api
+from avi import views_api
 
 api_urls = [
     # API definitions
@@ -14,12 +14,9 @@ api_urls = [
 
     url(r'^(?P<pk>[0-9]+)/$', views_api.SparkJobDetail.as_view(), name='sparkjob_detail'),
 
-    url(r'^job_data/(?P<job_id>[0-9]+)/$', views_api.JobData.as_view(), name='api_job_data'),
-
     url(r'^view_jobs/$', views_api.ViewJobsList.as_view(), name='api_view_jobs'),
 
     url(r'^view_jobs/(?P<pk>[0-9]+)/$', views_api.ViewJobsListDetail.as_view(), name='api_view_jobs_detail'),
-
 ]
 
 api_urls = format_suffix_patterns(api_urls)
@@ -30,12 +27,8 @@ urlpatterns = patterns(
 
     url(r'^job_list/', include(plugins_urls, namespace='plugins')),
 
-    url(r'^run_spark/$', views.run_spark, name='run_spark'),
+    # URLs with the next two names are required
+    url(r'^result/(?P<job_id>[0-9]+)/$', views_api.JobResult.as_view(), name='job_result'),
 
-    # Same as api-job-data above
-    url(r'^job_data/(?P<job_id>[0-9]+)/$', views_api.JobData.as_view(), name='job_data'),
-
-    url(r'^result/(?P<job_id>[0-9]+)/$', views.job_result, name='job_result'),
-
-    url(r'^public/result/(?P<job_id>[0-9]+)/(?P<celery_task_id>[a-z0-9-]+)/$', views.job_result_public, name='job_result_public'),
+    url(r'^public/result/(?P<job_id>[0-9]+)/(?P<celery_task_id>[a-z0-9-]+)/$', views_api.JobResultPublic.as_view(), name='job_result_public'),
 )
